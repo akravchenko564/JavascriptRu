f950135937e9b35b647d8a31dad54d58bd5c3686
akravchenko564 <akravchenko564@gmail.com>
2017-11-23 20:41:35 +0200

    2.10.3

---
+++ javascript.ru/2.10.3/2.10.3.js
@@ -0,0 +1,6 @@
+﻿var a = 2;
+var b = 3;
+alert('(a ^ b) == (b ^ a) is ' + ((a ^ b) == (b ^ a)));
+alert('(a ^ b) == (b ^ a) is ' + ((a ^ b) == (b ^ a)));
+alert('(a & b) == (b & a) is ' + ((a & b) == (b & a)));
+alert('(a | b) == (b | a) is ' + ((a | b) == (b | a)));
