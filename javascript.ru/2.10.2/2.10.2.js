bc362fd53eac0f2d706503622a2e6198c1aef3cc
akravchenko564 <akravchenko564@gmail.com>
2017-11-25 22:03:41 +0200

    2.10.2

---
+++ javascript.ru/2.10.2/2.10.2.js
@@ -0,0 +1,7 @@
+﻿function isInteger(num) {
+	return (num ^ 0) === num;
+}
+
+alert('isInteger(1) = ' + isInteger(1)); // true
+alert('isInteger(1.5) = ' + isInteger(1.5)); // false
+alert('isInteger(-0.5) = ' + isInteger(-0.5)); // false
