c251d36aed38769e40f51943c63d22f09be95faf
akravchenko564 <akravchenko564@gmail.com>
2017-12-01 14:13:13 +0200

    Tests.

--- javascript.ru/2.12.1/2.12.1.js
+++ javascript.ru/2.12.1/2.12.1.js
@@ -1,3 +1,15 @@
-﻿if ("0") {
-	alert('Привет');
+﻿if ('0') {
+	alert('if (0) = ' + '0');
+}
+
+if (0) {
+	alert('if (0) = ' + '1');
+}
+
+if ('') {
+	alert('if ( ) = ' + '2');
+}
+
+if (undefined) {
+	alert('if (undefined) = ' + '3');
 }
