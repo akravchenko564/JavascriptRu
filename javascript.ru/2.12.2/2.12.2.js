cbe8a982f1e00e96563f7f4e6a05381428c56e80
akravchenko564 <akravchenko564@gmail.com>
2017-12-01 14:19:08 +0200

    Changes.

--- javascript.ru/2.12.2/2.12.2.js
+++ javascript.ru/2.12.2/2.12.2.js
@@ -1,9 +1,9 @@
-﻿var value = prompt('Are the unicorns real', '');
+﻿var value = prompt('Are unicorns real?', '');
 
-if (value.toUpperCase() == 'Yes'.toUpperCase() ) {
-	alert('right!');
+if (value.toUpperCase() == 'Yes'.toUpperCase()) {
+	alert('Right!');
 }
 
 else {
-	alert(' It is not right. Of course they are real!');
+	alert('It is not right. Of course they are real!');
 }
