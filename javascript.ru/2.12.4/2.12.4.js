2ce9e1c72c335bbfe827fd8f560d0ecb348837e7
akravchenko564 <akravchenko564@gmail.com>
2017-12-03 23:14:22 +0200

    2.12.4

---
+++ javascript.ru/2.12.4/2.12.4.js
@@ -0,0 +1,22 @@
+﻿var user = prompt('Who are you?');
+
+if (user == 'Admin') {
+	var password = prompt('Enter password');
+	if (password == 'Loki') {
+		alert('Wellcome!');
+	}	
+	else if (password == 'Cancel') {
+		alert('Canceled');
+	}
+	else {
+		alert('Not correct password');
+	}
+}
+ 
+else if (user == 'Cancel') {
+	alert('Login canceled');
+}
+
+else {
+	alert('Unknown user');
+}
