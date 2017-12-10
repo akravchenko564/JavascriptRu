240e9003d5a833bd4c5aca90afcd65e804c27def
akravchenko564 <akravchenko564@gmail.com>
2017-12-10 19:51:06 +0200

    2.12.6

---
+++ javascript.ru/2.12.6/2.12.6.js
@@ -0,0 +1,5 @@
+﻿var login = prompt('Enter login');
+var message = (login.toLowerCase() == 'Vasya'.toLowerCase()) ? 'Hi' :
+	(login.toLowerCase() == 'Director'.toLowerCase()) ? 'Hello' :
+	(login == '') ? 'No login' : '';
+alert(message);
